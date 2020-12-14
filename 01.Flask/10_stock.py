36b18d3e7f5056e233f4feb586fe3f63fe512e7c
Cheolkie Kim <ckiekim@naver.com>
2020-12-14 10:49:01 +0900

    주가예측

--- 01.Flask/10_stock.py
+++ 01.Flask/10_stock.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, session, request
-from datetime import timedelta
+from fbprophet import Prophet
+from datetime import datetime, timedelta
+import os
+import pandas as pd
+import pandas_datareader as pdr
+import matplotlib as mpl 
+import matplotlib.pyplot as plt 
+# 한글폰트 사용
+mpl.rc('font', family='Malgun Gothic')
+mpl.rc('axes', unicode_minus=False)
+
 from my_util.weather import get_weather
 app = Flask(__name__)
 app.secret_key = 'qwert12345'
@@ -32,7 +42,30 @@ def stock():
     if request.method == 'GET':
         return render_template('10.stock.html', menu=menu, weather=get_weather_main())
     else:
-        return render_template('10.stock_res.html', menu=menu, mtime=1, weather=get_weather_main())
+        code = request.form['code']
+        learn_period = int(request.form['learn'])
+        pred_period = int(request.form['pred'])
+        today = datetime.now()
+        start_learn = today - timedelta(days=learn_period*365)
+        end_learn = today - timedelta(days=1)
+
+        stock_data = pdr.DataReader(code, data_source='yahoo', start=start_learn, end=end_learn)
+        app.logger.debug(f"get stock data: {code}")
+        df = pd.DataFrame({'ds': stock_data.index, 'y': stock_data.Close})
+        df.reset_index(inplace=True)
+        del df['Date']
+
+        model = Prophet(daily_seasonality=True)
+        model.fit(df)
+        future = model.make_future_dataframe(periods=pred_period)
+        forecast = model.predict(future)
+
+        fig = model.plot(forecast);
+        img_file = os.path.join(app.root_path, 'static/img/stock.png')
+        fig.savefig(img_file)
+        mtime = int(os.stat(img_file).st_mtime)
+
+        return render_template('10.stock_res.html', menu=menu, weather=get_weather_main(), mtime=mtime)
 
 if __name__ == '__main__':
     app.run(debug=True)
