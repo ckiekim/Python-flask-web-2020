e5cab030b0e5a7f91ad0240844fce9bcd003805d
Cheolkie Kim <ckiekim@naver.com>
2020-12-14 14:03:58 +0900

    주가예측

--- 01.Flask/10_stock.py
+++ 01.Flask/10_stock.py
@@ -13,6 +13,7 @@ mpl.rc('axes', unicode_minus=False)
 from my_util.weather import get_weather
 app = Flask(__name__)
 app.secret_key = 'qwert12345'
+kospi_dict, kosdaq_dict = {}, {}
 
 def get_weather_main():
     weather = None
@@ -25,6 +26,19 @@ def get_weather_main():
         session.permanent = True
         app.permanent_session_lifetime = timedelta(minutes=60)
     return weather
+
+@app.before_first_request
+def before_first_request():
+    kospi = pd.read_csv('./static/data/KOSPI.csv', dtype={'종목코드': str})
+    for i in kospi.index:
+        kospi_dict[kospi['종목코드'][i]] = kospi['기업명'][i]
+    kosdaq = pd.read_csv('./static/data/KOSDAQ.csv', dtype={'종목코드': str})
+    for i in kosdaq.index:
+        kosdaq_dict[kosdaq['종목코드'][i]] = kosdaq['기업명'][i]
+
+@app.before_request
+def before_request():
+    pass    # 모든 Get 요청을 처리하는 놈에 앞서서 공통적으로 뭔 일을 처리함
 
 @app.route('/')
 def index():
@@ -40,9 +54,18 @@ def park():
 def stock():
     menu = {'ho':0, 'da':1, 'ml':0, 'se':0, 'co':0, 'cg':0, 'cr':0, 'st':1, 'wc':0}
     if request.method == 'GET':
-        return render_template('10.stock.html', menu=menu, weather=get_weather_main())
+        return render_template('10.stock.html', menu=menu, weather=get_weather_main(),
+                                kospi=kospi_dict, kosdaq=kosdaq_dict)
     else:
-        code = request.form['code']
+        market = request.form['market']
+        if market == 'KS':
+            code = request.form['kospi_code']
+            company = kospi_dict[code]
+            code += '.KS'
+        else:
+            code = request.form['kosdaq_code']
+            company = kosdaq_dict[code]
+            code += '.KQ'
         learn_period = int(request.form['learn'])
         pred_period = int(request.form['pred'])
         today = datetime.now()
@@ -65,7 +88,8 @@ def stock():
         fig.savefig(img_file)
         mtime = int(os.stat(img_file).st_mtime)
 
-        return render_template('10.stock_res.html', menu=menu, weather=get_weather_main(), mtime=mtime)
+        return render_template('10.stock_res.html', menu=menu, weather=get_weather_main(), 
+                                mtime=mtime, company=company, code=code)
 
 if __name__ == '__main__':
     app.run(debug=True)
