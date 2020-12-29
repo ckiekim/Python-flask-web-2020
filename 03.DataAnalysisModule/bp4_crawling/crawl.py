ba97f42d7cd75fefbd10ba9ca3bb214664f40dbd
Cheolkie Kim <ckiekim@naver.com>
2020-12-29 14:10:13 +0900

    크롤링

--- 03.DataAnalysisModule/bp4_crawling/crawl.py
+++ 03.DataAnalysisModule/bp4_crawling/crawl.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template, request, session, g
-from flask import current_app
+from flask import Blueprint, render_template, request, session
+from flask import current_app, redirect, url_for
 from fbprophet import Prophet
 from datetime import datetime, timedelta
 import os
 import pandas as pd
 from my_util.weather import get_weather
+import my_util.crawl_util as cu
 
 crawl_bp = Blueprint('crawl_bp', __name__)
 
@@ -20,10 +21,28 @@ def get_weather_main():
         current_app.permanent_session_lifetime = timedelta(minutes=60)
     return weather
 
-@crawl_bp.route('/food')
+@crawl_bp.route('/food', methods=['GET', 'POST'])
 def food():
-    pass
+    menu = {'ho':0, 'da':1, 'ml':0, 'se':0, 'co':0, 'cg':0, 'cr':1, 'st':0, 'wc':0}
+    if request.method == 'GET':
+        place = request.args.get('place', '발산역')
+        rest_list = cu.siksin(place)
+        return render_template('crawling/food.html', menu=menu, weather=get_weather(),
+                                rest_list=rest_list, place=place)
+    else:
+        place = request.form['place']
+        return redirect(url_for('crawl_bp.food')+f'?place={place}')
 
 @crawl_bp.route('/music')
 def music():
-    pass
+    menu = {'ho':0, 'da':1, 'ml':0, 'se':0, 'co':0, 'cg':0, 'cr':1, 'st':0, 'wc':0}
+    music_list = cu.genie()
+    return render_template('crawling/music.html', menu=menu, weather=get_weather(),
+                            music_list=music_list)
+
+@crawl_bp.route('/book')
+def book():
+    menu = {'ho':0, 'da':1, 'ml':0, 'se':0, 'co':0, 'cg':0, 'cr':1, 'st':0, 'wc':0}
+    book_list = cu.interpark()
+    return render_template('crawling/book.html', menu=menu, weather=get_weather(),
+                            book_list=book_list)
