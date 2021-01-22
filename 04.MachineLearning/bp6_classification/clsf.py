a5a95820d6f11c3f6e6fe06d20c6590f8c5096a2
Cheolkie Kim <ckiekim@naver.com>
2021-01-22 14:57:58 +0900

    분류

--- 04.MachineLearning/bp6_classification/clsf.py
+++ 04.MachineLearning/bp6_classification/clsf.py
@@ -109,3 +109,59 @@ def cancer():
         org = dict(zip(df.columns[:-1], df.iloc[index, :-1]))
         return render_template('classification/cancer_res.html', menu=menu, 
                                 res=result, org=org, weather=get_weather())
+
+@clsf_bp.route('/iris', methods=['GET', 'POST'])
+def iris():
+    menu = {'ho':0, 'da':0, 'ml':10, 
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
+            'cf':1, 'ac':0, 're':0, 'cu':0}
+    if request.method == 'GET':
+        return render_template('classification/iris.html', menu=menu, weather=get_weather())
+    else:
+        index = int(request.form['index'])
+        df = pd.read_csv('static/data/iris_test.csv')
+        scaler = joblib.load('static/model/iris_scaler.pkl')
+        test_data = df.iloc[index, :-1].values.reshape(1,-1)
+        test_scaled = scaler.transform(test_data)
+        label = df.iloc[index, -1]
+        lrc = joblib.load('static/model/iris_lr.pkl')
+        svc = joblib.load('static/model/iris_sv.pkl')
+        rfc = joblib.load('static/model/iris_rf.pkl')
+        pred_lr = lrc.predict(test_scaled)
+        pred_sv = svc.predict(test_scaled)
+        pred_rf = rfc.predict(test_scaled)
+
+        species = ['Setosa', 'Versicolor', 'Virginica']
+        result = {'index':index, 'label':f'{label} ({species[label]})',
+                  'pred_lr':f'{pred_lr[0]} ({species[pred_lr[0]]})', 
+                  'pred_sv':f'{pred_sv[0]} ({species[pred_sv[0]]})', 
+                  'pred_rf':f'{pred_rf[0]} ({species[pred_rf[0]]})'}
+        org = dict(zip(df.columns[:-1], df.iloc[index, :-1]))
+        return render_template('classification/iris_res.html', menu=menu, 
+                                res=result, org=org, weather=get_weather())
+
+@clsf_bp.route('/wine', methods=['GET', 'POST'])
+def wine():
+    menu = {'ho':0, 'da':0, 'ml':10, 
+            'se':0, 'co':0, 'cg':0, 'cr':0, 'wc':0,
+            'cf':1, 'ac':0, 're':0, 'cu':0}
+    if request.method == 'GET':
+        return render_template('classification/wine.html', menu=menu, weather=get_weather())
+    else:
+        index = int(request.form['index'])
+        df = pd.read_csv('static/data/wine_test.csv')
+        scaler = joblib.load('static/model/wine_scaler.pkl')
+        test_data = df.iloc[index, :-1].values.reshape(1,-1)
+        test_scaled = scaler.transform(test_data)
+        label = df.iloc[index, -1]
+        lrc = joblib.load('static/model/wine_lr.pkl')
+        svc = joblib.load('static/model/wine_sv.pkl')
+        rfc = joblib.load('static/model/wine_rf.pkl')
+        pred_lr = lrc.predict(test_scaled)
+        pred_sv = svc.predict(test_scaled)
+        pred_rf = rfc.predict(test_scaled)
+        result = {'index':index, 'label':label,
+                  'pred_lr':pred_lr[0], 'pred_sv':pred_sv[0], 'pred_rf':pred_rf[0]}
+        org = dict(zip(df.columns[:-1], df.iloc[index, :-1]))
+        return render_template('classification/wine_res.html', menu=menu, 
+                                res=result, org=org, weather=get_weather())
